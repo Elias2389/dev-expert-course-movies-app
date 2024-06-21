3a4d5dba6f8d7eb6e845a0683a2c12afa953b07f
Elias2389 <andreselias2303@gmail.com>
2024-06-21 10:04:42 +0200

    initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,30 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(120.dp),
+                contentPadding = PaddingValues(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+            ) {
+
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/model/Movie.kt
@@ -0,0 +1,15 @@
+package model
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val poster: String,
+)
+
+val movies = (1 .. 100).map {
+    Movie(
+        id = it,
+        title = "Movie $it",
+        poster = "https://picsum.photos/id/$it/200/300",
+    )
+}
